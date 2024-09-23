eb811ec3b606da3964f8c70d36b847a9b91ce445
Darren Ng <>
2024-09-23 12:43:06 +0800

    TODO test

---
+++ rc.c
@@ -0,0 +1,47 @@
+#include <errno.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/utsname.h>
+
+extern const int argc;
+extern const char *const *const argv;
+
+int mmm_uname(){
+	struct utsname u={};
+	const int r=uname(&u);
+	if(0!=r)
+		return r;
+	printf("%s\n%s\n%s\n%s\n%s\n%s\n",
+		u.sysname,
+		u.nodename,
+		u.release,
+		u.version,
+		u.machine,
+		u.domainname
+	);
+	return 0;
+}
+
+int mmm_sqrt(){
+	if(!argv[2]){
+		puts("err no arg2");
+		return 1;
+	}
+	errno=0;
+	char *e=NULL;
+	double x=strtold(argv[2], &e);
+	if(0!=errno){
+		printf("err strtold errno %d %s\n", errno, strerror(errno));
+		return errno;
+	}
+	if(argv[2]==e) {
+		printf("err strtold no number detected in \'%s\'\n", argv[2]);
+		return 1;
+	}
+	// man math_error
+	printf("%lf\n", sqrt(x));
+	return 0;
+}
+
